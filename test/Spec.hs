2969d38e5f65a556f4623e2c62db060e376289d9
youkaichao <youkaichao@126.com>
2019-05-16 15:12:55 +0800

    add many test cases

--- test/Spec.hs
+++ test/Spec.hs
@@ -20,17 +20,18 @@ test_sum3 =
   makeFun ("sum3", TInt) [("x", TInt), ("y", TInt), ("z", TInt)] (EAdd (EAdd (EVar "x")(EVar "y")) (EVar "z")) $ callFun (EVar "sum3") [EIntLit 1, EIntLit 2, EIntLit 3]
 
 test_fbi =
-  Program [] $
-
   makeFun ("fbi", TInt) [("x", TInt)] (
   EIf (ELe (EVar "x") (EIntLit 1))
     (EIntLit 1)
     (EAdd
       (EApply (EVar "fbi") (ESub (EVar "x") (EIntLit 1)))
       (EApply (EVar "fbi") (ESub (EVar "x") (EIntLit 2))))
-  ) $
+  ) $ callFun (EVar "fbi") [EIntLit 10]
 
-  callFun (EVar "fbi") [EIntLit 10]
+test_stuck =
+  makeFun ("stuck", TBool) [("x", TBool)] (
+    ENot (EApply (EVar "stuck") (EVar "x")))
+  $ callFun (EVar "stuck") [EBoolLit True]
 
 test_adt_ctor =
   Program
@@ -113,34 +114,59 @@ test_adt_list_sum =
   ELet ("l", callFun (EVar "range") [EIntLit 100]) $
   callFun (EVar "sum") [EVar "l"]
 
-test_data :: [(Expr, Maybe Type, Maybe Value)]
+test_data :: [(Expr, Maybe Type, AST.Result)]
 test_data = 
   [
-    (EBoolLit True, Just TBool, Just $ VBool True),
-    (EBoolLit False, Just TBool, Just $ VBool False),
-    (EIntLit 1, Just TInt, Just $ VInt 1),
-    (EIntLit (-1), Just TInt, Just $ VInt (-1)),
-    (EAnd (EBoolLit True) (EBoolLit False), Just TBool, Just $ VBool False),
-    (EAdd (EIntLit 1) (EIntLit 2), Just TInt, Just $ VInt 3),
-    (EDiv (EIntLit 10) (EIntLit 2), Just TInt, Just $ VInt 5),
-    (EEq (EIntLit 1) (EIntLit 2), Just TBool, Just $ VBool False),
-    (ENeq (EIntLit 1) (EIntLit 2), Just TBool, Just $ VBool True),
-    (EIf (EBoolLit True) (EIntLit 1) (EIntLit 2), Just TInt, Just $ VInt 1),
-    (EIf (EBoolLit False) (EIntLit 1) (EIntLit 2), Just TInt, Just $ VInt 2),
-    (ELet ("a", (EIntLit 1)) (EAdd (EVar "a") (EIntLit 1)), Just TInt, Just $ VInt 2),
-    (EApply (ELambda ("x",TBool) (ENot (EVar "x"))) (EBoolLit True),
-      Just TBool, Just $ VBool False
-    ),
-    (EApply (ELambda ("x",TInt) (EAdd (EVar "x") (EIntLit 1))) (EIntLit 1),
-      Just TInt, Just $ VInt 2
-    )
+    (EBoolLit True, Just TBool, RBool True),
+    (EBoolLit False, Just TBool, RBool False),
+    (EIntLit 1, Just TInt, RInt 1),
+    (EIntLit (-1), Just TInt, RInt (-1)),
+    (ECharLit 'a', Just TChar, RChar 'a'),
+    (ECharLit '1', Just TChar, RChar '1'),
+    (ENot $ ECharLit '1', Nothing, RInvalid),
+    (ENot $ EBoolLit True, Just TBool, RBool False),
+    (ENot $ EBoolLit False, Just TBool, RBool True),
+    (EAnd (EBoolLit True) (EBoolLit False), Just TBool, RBool False),
+    (EAnd (EBoolLit False) test_stuck, Just TBool, RBool False), -- 短路求值
+    (EOr (EBoolLit True) (EBoolLit False), Just TBool, RBool True),
+    (EOr (EBoolLit True) test_stuck, Just TBool, RBool True), -- 短路求值
+    (EAdd (EIntLit 1) (EIntLit 2), Just TInt, RInt 3),
+    (ESub (EIntLit 1) (EIntLit 2), Just TInt, RInt (-1)),
+    (EMul (EIntLit 1) (EIntLit 2), Just TInt, RInt 2),
+    (EDiv (EIntLit 10) (EIntLit 2), Just TInt, RInt 5),
+    (EEq (EIntLit 1) (EIntLit 2), Just TBool, RBool False),
+    (EEq (EBoolLit True) (EBoolLit True), Just TBool, RBool True),
+    (EEq (ECharLit 'a') (ECharLit 'b'), Just TBool, RBool False),
+    (ENeq (EIntLit 1) (EIntLit 2), Just TBool, RBool True),
+    (ENeq (EBoolLit True) (EBoolLit True), Just TBool, RBool False),
+    (ENeq (ECharLit 'a') (ECharLit 'b'), Just TBool, RBool True),
+    (ELt (EIntLit 1) (EIntLit 2), Just TBool, RBool True),
+    (ELt (EIntLit 2) (EIntLit 1), Just TBool, RBool False),
+    (EGt (EIntLit 1) (EIntLit 2), Just TBool, RBool False),
+    (EGt (EIntLit 2) (EIntLit 1), Just TBool, RBool True),
+    (ELe (EIntLit 1) (EIntLit 2), Just TBool, RBool True),
+    (ELe (EIntLit 2) (EIntLit 1), Just TBool, RBool False),
+    (ELe (EIntLit 2) (EIntLit 2), Just TBool, RBool True),
+    (EGe (ECharLit '1') (ECharLit '2'), Just TBool, RBool False),
+    (EGe (ECharLit '2') (ECharLit '1'), Just TBool, RBool True),
+    (EGe (ECharLit '2') (ECharLit '2'), Just TBool, RBool True),
+    (EIf (EBoolLit True) (EIntLit 1) (EIntLit 2), Just TInt, RInt 1),
+    (EIf (EBoolLit False) (EIntLit 1) (EIntLit 2), Just TInt, RInt 2),
+    (EIf (EBoolLit True) (EBoolLit False) (test_stuck), Just TBool, RBool False),
+    (EIf (EBoolLit False) (test_stuck) (EBoolLit False), Just TBool, RBool False),
+    (ELambda ("x",TBool) (ENot (EVar "x")), Just $ TArrow TBool TBool, RInvalid),
+    (ELet ("a", (EIntLit 1)) (EAdd (EVar "a") (EIntLit 1)), Just TInt, RInt 2),
+    (EApply (ELambda ("x",TBool) (ENot (EVar "x"))) (EBoolLit True),Just TBool, RBool False),
+    (EApply (ELambda ("x",TInt) (EAdd (EVar "x") (EIntLit 1))) (EIntLit 1),Just TInt, RInt 2),
+    (test_fbi, Just TInt, RInt 89),
+    (ECase (EIntLit 3) [(PIntLit 2, EIntLit 1), (PVar "x", EAdd (EVar "x") (EIntLit 1))], Just TInt, RInt 4)
   ]
 
 (programs, expected_types, expected_values) = unzip3 test_data
 
 func (eachT, program) = case eachT of 
-  Just x -> evalProgram (Program [] program) -- well typed, 求值
-  Nothing -> Nothing
+  Just x -> evalValue (Program [] program) -- well typed, 求值
+  Nothing -> RInvalid
 
 types = map evalType $ map (\x -> Program [] x) programs
 values = map func $ zip types programs
