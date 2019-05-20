9e0034f657c561c4dc996942b71ba06ec6c59608
youkaichao <youkaichao@126.com>
2019-05-20 16:13:55 +0800

    compiler done

---
+++ src/EvalJS.hs
@@ -0,0 +1,46 @@
+-- | 这是其中一种实现方式的代码框架。你可以参考它，或用你自己的方式实现，只要按需求完成 evalJS :: Program -> String 就行。
+module EvalJS where
+
+import Text.Printf
+import Data.List
+import AST
+import Control.Monad.State
+import qualified EvalType
+
+eval :: Expr -> String
+eval (EBoolLit b) = if b then "true" else "false"
+eval (EIntLit b) = show b
+eval (ECharLit b) = b:[]
+eval (ENot e) = printf "! %s" $ eval e
+eval (EAnd a b) = printf "!!(%s) && !!(%s)" (eval a) (eval b)
+eval (EOr a b) = printf "!!(%s) || !!(%s)" (eval a) (eval b)
+eval (EAdd a b) = printf "(%s) + (%s)" (eval a) (eval b)
+eval (ESub a b) = printf "(%s) - (%s)" (eval a) (eval b)
+eval (EMul a b) = printf "(%s) * (%s)" (eval a) (eval b)
+eval (EDiv a b) = printf "Math.floor((%s) / (%s))" (eval a) (eval b)
+eval (EMod a b) = printf "(%s) %% (%s)" (eval a) (eval b)
+eval (EEq a b) =  printf "(%s) === (%s)" (eval a) (eval b)
+eval (ENeq a b) = printf "(%s) !== (%s)" (eval a) (eval b)
+eval (ELt a b) =  printf "(%s) < (%s)" (eval a) (eval b)
+eval (EGt a b) =  printf "(%s) > (%s)" (eval a) (eval b)
+eval (ELe a b) =  printf "(%s) <= (%s)" (eval a) (eval b)
+eval (EGe a b) =  printf "(%s) >= (%s)" (eval a) (eval b)
+eval (EIf a b c) = printf "if(%s){%s}else{%s}" (eval a) (eval b) (eval c)
+eval (ELambda (s, t) e) = printf "function (%s) {return (%s)}" s (eval e)
+eval (ELet (s, a) b) = printf "(function (%s) {return (%s)})(%s)" s (eval b) (eval a)
+eval (ELetRec func (arg, targ) (e, te) exp) = printf "(function (%s){return %s})(function (%s){return %s})" func (eval exp) arg (eval e)
+eval (EVar s) = s
+eval (EApply a b) = printf "(%s)(%s)" (eval a) (eval b)
+eval (ECase e pes) = let tmp_name = "$_tmp" in printf "{%s=(%s);%s}" tmp_name (eval e) (concat $ pattern tmp_name pes)
+
+pattern :: [Char] -> [(Pattern, Expr)] -> [[Char]]
+pattern tmp_name [] = ["{undefined}"]
+pattern tmp_name ((p, e):xs) = case p of
+  PBoolLit b -> (printf "if(%s===%s){%s}else " tmp_name (if b then "true" else "false") (eval e)) : pattern tmp_name xs
+  PIntLit b -> (printf "if(%s===%s){%s}else " tmp_name (show b) (eval e)) : pattern tmp_name xs
+  PCharLit b -> (printf "if(%s===%s){%s}else " tmp_name (b:[]) (eval e)) : pattern tmp_name xs
+  PVar b -> (printf "if(%s===%s){%s=%s;%s}else " tmp_name tmp_name b tmp_name (eval e)) : pattern tmp_name xs
+    
+
+evalJS :: Program -> String
+evalJS (Program adts body) = eval body
